5618d0536ec435c17fbc62234b287b52ef101eff
justgithubaccount <kulikovyevgeny@outlook.com>
2023-12-04 19:12:00 +0300

    Adding Ansible config files

---
+++ iac/ansible/ansible-basic.sh
@@ -0,0 +1,41 @@
+# https://github.com/justgithubaccount/welcome-ansible
+
+# run playbook
+ansible-playbook jenkins-and-docker.yaml -l jenkins --check
+ansible jenkins -m shell -a "cat /var/lib/jenkins/secrets/initialAdminPassword"
+
+# basic
+
+ansible-doc -l | grep web
+
+ansible jenkins -m ping
+
+ansible jenkins -m setup
+
+ansible jenkins -m shell -a "cat /etc/os-release"
+ansible jenkins -m shell -a "hostname && hostname -I && uptime"
+
+ansible jenkins -m shell -a "hostnamectl set-hostname jenkins"
+ansible jenkins -m shell -a "apt update -y"
+ansible jenkins -m shell -a "reboot"
+
+ansible all -m shell -a "ls /home"
+
+ansible jenkins -m copy -a "src=jenkins-and-docker.yaml dest=/home mode=664" -b
+
+ansible jenkins -m file -a "path=/home/jenkins-and-docker.yaml state=absent" 
+ansible jenkins -m file -a "path=/home/ansible.cfg state=absent"
+
+ansible jenkins -m get_url -a "url=https://td.telegram.org/tlinux/tsetup.4.12.2.tar.xz dest=/home"
+
+ansible jenkins -m apt -a "name=fontconfig state=latest" -b
+ansible jenkins -m apt -a "name=fontconfig state=removed" -b
+
+ansible jenkins -m apt -a "name=httpd state=removed" -b
+ansible jenkins -m service -a "name=httpd state=started enabled=yes" -b
+
+ansible jenkins -m shell -a "cat /etc/os-release" -v
+ansible jenkins -m shell -a "cat /etc/os-release" -vv
+ansible jenkins -m shell -a "cat /etc/os-release" -vvv
+ansible jenkins -m shell -a "cat /etc/os-release" -vvvv
+ansible jenkins -m shell -a "cat /etc/os-release" -vvvvv
