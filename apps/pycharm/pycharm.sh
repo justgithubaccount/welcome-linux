2cb7982b5dba30093c5a6151908baa6ef94b7b17
justgithubaccount <kulikovyevgeny@outlook.com>
2023-12-20 10:43:56 +0300

    Update apps and users

---
+++ apps/pycharm/pycharm.sh
@@ -0,0 +1,9 @@
+wget https://download-cdn.jetbrains.com/python/pycharm-community-2023.3.1.tar.gz
+
+tar -xf ~/Downloads/pycharm-community-2023.3.1.tar.gz
+
+# ./Applications/pycharm-community-2023.3.1/bin/pycharm.sh  
+
+mv jetbrains-pycharm.desktop ~/.local/share/applications/jetbrains-pycharm.desktop
+
+chmod +x ~/.local/share/applications/jetbrains-pycharm.desktop
