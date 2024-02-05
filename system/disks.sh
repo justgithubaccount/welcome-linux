6e51195f02ca7eee925c7f03eb4715bbe51e5158
justgithubaccount <kulikovyevgeny@outlook.com>
2024-02-05 17:44:43 +0300

    Update disks

--- system/disks.sh
+++ system/disks.sh
@@ -1,6 +1,31 @@
+# sd* (scsi диск)
 ls /dev/sd*
+# cr* (cd приводы)
 ls /dev/sr*
+# Програмка для удобного просмотра дисков
 lsscsi -s
-sudo fdisk /dev/sdb
+
+# ОС обменивается с этими устройствами данными в виде блоков данных фиксированной длины
+# b - блочное устройство
+ls -l /dev/sda
+stat /dev/sda
+
+# c - character device – символьное устройство
+ls -l /dev/input/mouse0
+stat /dev/input/mouse0
+
+# fdisk - для работы с дисками (и parted)
+sudo fdisk /dev/sda # n - t - w
+sudo fdisk -l /dev/sda # Информация о разделе
+sudo mkfs.ext4 /dev/sda1 # Форматнуть раздел  
+sudo mkfs.ext4 /dev/sda2 
+sudo mkfs.ext4 /dev/sda3
+
+# Псевдограффика
+sudo cfdisk /dev/sda
+
+# Блочные устройства в древовидной форме
 lsblk 
+
+# Идентификаторы дисков
 ll -R /dev/disk
